3f53f44eb9cc66e07a48d15d967c99f1e5bf7bcc
Andrew Smith <andrew@andrewleesmith.co.uk>
2020-04-20 11:13:03 +0100

    Exception handling changed

--- aws-iot-lambda-rds/db_insert.py
+++ aws-iot-lambda-rds/db_insert.py
@@ -1,46 +1,30 @@
 #
-#Lambda function used to write inbound IoT sensor data to RDS PostgeSQL database.
+#Lambda function used to write inbound IoT sensor data to RDS PostgeSQL database
 #
 import sys
 import os
 import json
-import logging
 import psycopg2
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def make_connection():
-    conn = psycopg2.connect(host=os.environ['rds_host'], port=os.environ['rds_port'], dbname=os.environ['rds_dbname'], 
-                            user=os.environ['rds_username'], password=os.environ['rds_password'])
-    conn.autocommit = True
-    return conn
-
-logger.info("Cold start complete.")
-
+#Connect to PostgreSQL database and insert sensor data record
 def handler(event, context):
 
     try:
-        conn = make_connection()
+        conn = psycopg2.connect(host=os.environ['rds_host'], port=os.environ['rds_port'], 
+                                dbname=os.environ['rds_dbname'], user=os.environ['rds_username'], 
+                                password=os.environ['rds_password'])
+        conn.autocommit = True
+    
         cur = conn.cursor()
+            
+        cur.execute('insert into "SensorData" ("DeviceID", "DateTime", "Temperature", "Humidity", ' 
+                    '"WindDirection", "WindIntensity", "RainHeight") values (%s, %s, %s, %s, %s, %s, %s)', 
+                    (event['deviceid'], event['datetime'], event['temperature'], event['humidity'], 
+                     event['windDirection'], event['windIntensity'], event['rainHeight']))
+        cur.close()
         
-        try:
-            cur.execute('insert into "SensorData" ("DeviceID", "DateTime", "Temperature", "Humidity", ' 
-                        '"WindDirection", "WindIntensity", "RainHeight") values (%s, %s, %s, %s, %s, %s, %s)', 
-                        (event['deviceid'], event['datetime'], event['temperature'], event['humidity'], 
-                         event['windDirection'], event['windIntensity'], event['rainHeight']))
-            cur.close()
-            
-        except:
-            logger.exception("ERROR: Cannot execute cursor.")
-            return
-            
-        return "Successfully inserted 1 row."
-                
-    except:
-        logger.exception("ERROR: Cannot connect to database.")
-        return
-            
+    #No except statement is used since any exceptions should fail the function so that the
+    #failed message is sent to the SQS destination configured for the Lambda function
     finally:
         try:
             conn.close()
@@ -50,4 +34,3 @@ def handler(event, context):
 #Used when testing from the Linux command line    
 #if __name__== "__main__":
 #    handler(None, None)
-    
